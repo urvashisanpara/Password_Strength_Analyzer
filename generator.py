919df2416de177ef00059f19bbe8817e56757106
Sanpara Urvashi <urvashisanpara@gmail.com>
2026-06-29 15:46:07 +0530

    Add password generator function

---
+++ generator.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def generate_password(length=16):
+    characters = (
+        string.ascii_letters +
+        string.digits +
+        "!@#$%^&*()"
+    )
+
+    password = ''.join(random.choice(characters) for _ in range(length))
+
+    return password
